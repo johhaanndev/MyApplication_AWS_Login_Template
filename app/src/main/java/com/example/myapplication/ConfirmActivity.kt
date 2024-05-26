74903af885d6b51af135d19c9daf119ef5f96c91
johhaanndev <71643284+johhaanndev@users.noreply.github.com>
2024-05-26 14:43:52 +0200

    create Sign In and Confirm Account Activities

---
+++ app/src/main/java/com/example/myapplication/ConfirmActivity.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ConfirmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirm)
+    }
+}
